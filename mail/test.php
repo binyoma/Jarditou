4f94c5f7d2a7db72d1cd057d00032490afa5a5cd
binyoma <80010-84-12@exchange.ad.afpanet>
2020-07-01 16:48:04 +0200

    mail

---
+++ mail/test.php
@@ -0,0 +1,10 @@
+<?php 
+
+mail("dave.loper@afpa.com", 
+"Confirmation d'inscription",
+ "Bienvenue sur Jarditou ! 
+ Tu peux y acheter des tomates cerises pour l'apéro et une brouette pour les transporter. 
+ Sors vite ton American Express !",
+  array("From" => "contact@jarditou.com", "Reply-To" => "commercial@jarditou.com", "X-Mailer" => "PHP/".phpversion() ));
+
+  ?>
