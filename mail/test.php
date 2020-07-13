2167df44b984874b53ebe5e111624840f86d387c
binyoma <80010-84-12@exchange.ad.afpanet>
2020-07-13 09:20:20 +0200

    mail

--- mail/test.php
+++ mail/test.php
@@ -1,10 +1,10 @@
 <?php 
 
-mail("dave.loper@afpa.com", 
-"Confirmation d'inscription",
- "Bienvenue sur Jarditou ! 
- Tu peux y acheter des tomates cerises pour l'apéro et une brouette pour les transporter. 
- Sors vite ton American Express !",
-  array("From" => "contact@jarditou.com", "Reply-To" => "commercial@jarditou.com", "X-Mailer" => "PHP/".phpversion() ));
 
+mail("dave.loper@afpa.com", 
+"Confirmation d'inscription", 
+"Bienvenue sur Jarditou ! Tu peux y acheter des tomates cerises pour l'apéro et une brouette pour les transporter. Sors vite ton American Express !", 
+array("From" => "contact@jarditou.com", 
+"Reply-To" => "commercial@jarditou.com",
+ "X-Mailer" => "PHP/".phpversion() ));
   ?>
